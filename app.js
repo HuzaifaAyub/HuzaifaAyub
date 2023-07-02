8f77f1131a8efebea0195ec4c1503096c8b13fad
Huzaifa Ayub <69161439+HuzaifaAyub@users.noreply.github.com>
2023-07-02 03:58:03 -0700

    Javascript

---
+++ app.js
@@ -0,0 +1,103 @@
+var a = 3
+if (a > 5) {
+    console.log("a>5");
+} else if (a < 5) {
+    console.log("a<5");
+} else {
+    console.log("a=5");
+}
+
+
+
+
+
+
+ var marks = 90;
+if (marks<=90)
+ {console.log("A");
+    }
+    else if (marks<=80) {console.log("B");
+        
+    } else if (marks<=70) {console.log("C");
+        }
+        else if (marks<=60) {
+            console.log("D");
+        }
+        else if (marks<=50){
+            console.log("E");
+        }
+         else 
+        {console.log("F");
+    //     }
+    //     function grading(marks){
+    //         return
+    //     }
+        
+        
+    //     function findEvenOdd(a)
+    //     {
+    //         if(a % 2 == 0 ){
+    //             console.log(a +  "is even")
+    //         }
+    //         else {
+    //             console.log(a +  "is odd") 
+    //         }
+    //     }
+    //     findEvenOdd (5);
+    //     findEvenOdd (8);
+    //     findEvenOdd (79);
+    //            // for( var i=0; i<=10; i++ )
+    //     // {
+    //     //     console.log("i");
+    //     // }
+
+    //     function checkletters(letters)
+    //     {
+    //         if(letters ==="a" || letters ==="e"|| letters==="i"|| letters==="o" || letters==="u"){
+    //             console.log( letters+"is a vowel");
+    //         }
+    //         else if ( letters<="a" || letters >= "z"){
+    //             console.log(letters+"is a consnats");
+    //         }
+    //         else{
+    //             console.log(letters+"not a letter");
+    //         }
+    //     }
+        
+
+        
+    //      console.log
+    //      checkletters (a);
+        
+        
+        
+    //     // var  yeardays = 
+    //     // (yeardays == 366 ){
+    //         // console.log(year+  "is a leap year");
+    //     // }
+    //         // else if (yeardays != 366) {
+    // //             // console.log
+    // //             (year+  "is not a leap year");
+    // //         }
+    // //   function identifying(year){
+    // //     return
+    // //   }
+    //         var obj = {
+    //             name :"Huzaifa",
+    //             fathername : "Ayub",
+    //             phone: 32442
+    //             ,
+    //            }
+               { function changetext(){
+                    var p = document.getElementsByTagName("p")[0];
+                    p.innertext = 'text is replaced';
+                  console.log (p);
+                }
+                }
+function changecolour(){ 
+    var div = document.getElementsByClassName('div');
+    for(
+        i=0; 
+    )
+
+}
